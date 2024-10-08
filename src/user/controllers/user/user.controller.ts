785ec749632fe01820104158d690d348cd258c6b
sovannarapov <sovannarapov9@gmail.com>
2024-10-08 15:46:25 +0700

    feat: add swagger api response

--- src/user/controllers/user/user.controller.ts
+++ src/user/controllers/user/user.controller.ts
@@ -16,7 +16,7 @@ import {
 import { UserService } from '../../services/user/user.service';
 import { UserDto } from '../../dtos/responses';
 import { Pagination } from 'nestjs-typeorm-paginate';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Permissions } from '../../decorators';
 import { PermissionGuard } from '../../securities/permissions.security';
 import { Response } from '../../../global/payload/responses/Response';
@@ -32,6 +32,10 @@ export class UserController {
   @UseGuards(PermissionGuard)
   @Get('page')
   @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    type: Response<Pagination<UserDto>>,
+    description: 'Get all role with pagination',
+  })
   async getAllRoleWithPagination(
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
     @Query('size', new DefaultValuePipe(10), ParseIntPipe) size: number = 10,
@@ -44,6 +48,10 @@ export class UserController {
   @Permissions('user::read')
   @UseGuards(PermissionGuard)
   @Get()
+  @ApiResponse({
+    type: Response<UserDto[]>,
+    description: 'Get the users',
+  })
   @HttpCode(HttpStatus.OK)
   getAllUsers(): Promise<Response<UserDto[]>> {
     return this.userService.getAllUsers();
@@ -53,6 +61,10 @@ export class UserController {
   @UseGuards(PermissionGuard)
   @Get(':id')
   @HttpCode(HttpStatus.OK)
+  @ApiResponse({
+    type: Response<UserDto>,
+    description: 'Get the user by id',
+  })
   getUserById(@Param('id') id: string): Promise<Response<UserDto>> {
     return this.userService.getUserById(id);
   }
@@ -60,6 +72,10 @@ export class UserController {
   @Permissions('user::write')
   @UseGuards(PermissionGuard)
   @Post()
+  @ApiResponse({
+    type: Response<UserDto>,
+    description: 'Create a new user',
+  })
   createUser(@Body() createUserDto: CreateUserDto): Promise<Response<UserDto>> {
     return this.userService.createUser(createUserDto);
   }
@@ -67,6 +83,10 @@ export class UserController {
   @Permissions('user::update')
   @UseGuards(PermissionGuard)
   @Patch(':id')
+  @ApiResponse({
+    type: Response<UserDto>,
+    description: 'Update the user by id',
+  })
   updateUser(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -77,6 +97,9 @@ export class UserController {
   @Permissions('user::delete')
   @UseGuards(PermissionGuard)
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiResponse({
+    description: 'Delete the user by id',
+  })
   @Delete(':id')
   async deleteUserById(@Param('id') id: string): Promise<Response<string>> {
     await this.userService.deleteUserById(id);
