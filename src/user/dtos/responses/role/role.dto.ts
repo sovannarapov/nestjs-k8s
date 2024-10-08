69b32024f0e66457724b9154cc954eb8115eb485
sovannarapov <sovannarapov9@gmail.com>
2024-10-08 15:51:16 +0700

    feat: add swagger api property

--- src/user/dtos/responses/role/role.dto.ts
+++ src/user/dtos/responses/role/role.dto.ts
@@ -1,24 +1,33 @@
 import { AutoMap } from '@automapper/classes';
 import { PermissionDto } from '../permission/permission.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RoleDto {
+  @ApiProperty()
   @AutoMap()
   id: string;
 
+  @ApiProperty()
   @AutoMap()
   name: string;
 
+  @ApiProperty()
   @AutoMap()
   description: string;
+
+  @ApiProperty()
   @AutoMap()
   isActive: boolean;
 
+  @ApiProperty()
   @AutoMap()
   createdAt: Date;
 
+  @ApiProperty()
   @AutoMap()
   updatedAt: Date;
 
+  @ApiProperty()
   @AutoMap()
   permissions: PermissionDto;
 }
